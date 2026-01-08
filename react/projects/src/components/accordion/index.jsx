4fe4e722d9951f8b769d3c130b3c15facf67ecf7
xristos <christos3214@gmail.com>
2026-01-08 15:38:55 +0200

    Added extra future: multiple selection

--- react/projects/src/components/accordion/index.jsx
+++ react/projects/src/components/accordion/index.jsx
@@ -6,27 +6,50 @@ import './styles.css';
 
 export default function Accordion(){
     const [selected,setSelected]=useState(null);
+    const [enableMultiSelection,setEnableMultiSelection]=useState(false);
+    const [multiple,setMultiple] = useState([]);
 
     function handleSingleSelection(getCurrentId){
         setSelected(getCurrentId === selected ? null : getCurrentId);
     }
 
-    console.log(selected);
+    function handleMultiSelection(getCurrentId){
+        let copyMultiple = [...multiple];
+        const findIndexOfCurrentId = copyMultiple.indexOf(getCurrentId);
+
+        console.log(findIndexOfCurrentId);
+        if(findIndexOfCurrentId === -1){
+            copyMultiple.push(getCurrentId);
+        }else{
+            copyMultiple.splice(findIndexOfCurrentId, 1);
+        }
+        setMultiple(copyMultiple);
+    }
 
     return (<div className="wrapper">
+        <button onClick={()=>setEnableMultiSelection(!enableMultiSelection)}>
+            Enable Multi Selection
+        </button>
         <div className="Accordion">
             {
                 data && data.length>0 ?
                     data.map(dataItem=>
                         <div className="item">
-                            <div className="title" onClick={()=>handleSingleSelection(dataItem.id)}>
+                            <div className="title" onClick={
+                                enableMultiSelection
+                                    ? ()=>handleMultiSelection(dataItem.id)
+                                    : ()=>handleSingleSelection(dataItem.id)
+                            }>
                                 <h3>{dataItem.title}</h3>
                                 <span>+</span>
                             </div>
 
-                            {selected === dataItem.id ?
-                                <div className="content">{dataItem.content}</div>
-                                : null}
+                            {enableMultiSelection
+                                    ? multiple.indexOf(dataItem.id) !== -1 &&(
+                                        <div className="content">{dataItem.content}</div>)
+                                    : selected === dataItem.id &&(
+                                        <div className="content">{dataItem.content}</div>)
+                            }
                         </div>)
                     : <div>No Data Found</div>
             }
