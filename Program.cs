ae96f18174c158b04e44e038d403baaba4121cb0
xristos <christos3214@gmail.com>
2026-02-23 19:24:07 +0200

    Very basic data manipulation and console logging for C#

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿string studentName = "Sophia Johnson";
+string course1Name = "English 101";
+string course2Name = "Algebra 101";
+string course3Name = "Biology 101";
+string course4Name = "Computer Science I";
+string course5Name = "Psychology 101";
+
+int course1Credit =3;
+int course2Credit =3;
+int course3Credit =4;
+int course4Credit =4;
+int course5Credit =3;
+
+int gradeA =4;
+int gradeB = 3;
+
+int course1Grade = gradeA;
+int course2Grade = gradeB;
+int course3Grade = gradeB;
+int course4Grade = gradeB;
+int course5Grade = gradeA;
+
+int totalCreditHours = course1Credit+course2Credit+course3Credit+course4Credit+course5Credit;
+int totalGradePoints = 0;
+totalGradePoints += course1Grade * course1Credit;
+totalGradePoints += course2Grade * course2Credit;
+totalGradePoints += course3Grade * course3Credit;
+totalGradePoints += course4Grade * course4Credit;
+totalGradePoints += course5Grade * course5Credit;
+
+decimal gradePointAverage = (decimal) totalGradePoints / totalCreditHours;
+Console.WriteLine($"Final GPA: {gradePointAverage}");
+
+//hold the leading integer
+int leadingDigit = (int) gradePointAverage;
+//hold the first digit after decimal part
+int firstDigit = (int) (gradePointAverage * 10) % 10;
+//hold the second digit after decimal part
+int secondDigit = (int) (gradePointAverage * 100) % 10;
+
+Console.WriteLine($"Student: {studentName}\n");
+Console.WriteLine("Course\t\t\tGrade\tCredit Hours");
+Console.WriteLine($"{course1Name}\t\t{course1Grade}\t\t{course1Credit}");
+Console.WriteLine($"{course2Name}\t\t{course2Grade}\t\t{course2Credit}");
+Console.WriteLine($"{course3Name}\t\t{course3Grade}\t\t{course3Credit}");
+Console.WriteLine($"{course4Name}\t{course4Grade}\t\t{course4Credit}");
+Console.WriteLine($"{course5Name}\t\t{course5Grade}\t\t{course5Credit}");
+Console.WriteLine($"Final GPA: {leadingDigit}.{firstDigit}{secondDigit}");
