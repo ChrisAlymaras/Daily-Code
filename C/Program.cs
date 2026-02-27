1ad0313b01f21bd87771ed9473d8a20deaced708
Chris Alymaras <112636528+ChrisAlymaras@users.noreply.github.com>
2026-02-27 12:20:40 +0200

    Add files via upload
    Some very simple if-else logic to move fast with the fundamentals of C#

--- C/Program.cs
+++ C/Program.cs
@@ -1,13 +1,35 @@
-﻿// generate a random number
-//Random dice  = new Random();
-//int roll = dice.Next(1,7);
+﻿Random dice = new Random();
 
-//Console.WriteLine(roll);
+int roll1 = dice.Next(1,7);
+int roll2 = dice.Next(1,7);
+int roll3 = dice.Next(1,7); 
 
-//larger value
-int firstValue = 500;
-int secondValue = 600;
+int total = roll1 + roll2 + roll3;
+roll1 = 6;
+roll2 = 6;
+roll3 = 5;
+Console.WriteLine($"Dice rolls: {roll1}+{roll2}+{roll3} Total sum is {total}");
 
-int largerValue;
-largerValue = System.Math.Max(firstValue,secondValue);
-Console.WriteLine(largerValue);
+if (total < 14)
+{
+    Console.WriteLine("You Win!");
+}
+else
+{
+    Console.WriteLine("Sorry you lose.");
+}
+
+//give a bonus to doubles
+if (roll1 == roll2 || roll1 ==roll3 || roll2 == roll3)
+{
+    if ((roll1 == roll2) && (roll2 == roll3))
+    {
+        Console.WriteLine("You rolled triples! +6 bonus to total!");
+        total += 6;
+    }
+    else
+    {
+        Console.WriteLine("You rolled doubles ! +2 bonus total !");
+        total +=2;    
+    }
+}
