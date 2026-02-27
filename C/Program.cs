2bbabb555c43935e7ba94fe456bb437ff1c2b3ea
Chris Alymaras <112636528+ChrisAlymaras@users.noreply.github.com>
2026-02-27 12:51:52 +0200

    Add files via upload
    Very simple if-logic and logic operators for subscription management. Console logging some messages.

--- C/Program.cs
+++ C/Program.cs
@@ -1,35 +1,33 @@
-﻿Random dice = new Random();
+﻿Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
 
-int roll1 = dice.Next(1,7);
-int roll2 = dice.Next(1,7);
-int roll3 = dice.Next(1,7); 
+// Your code goes here
+string message = "";
 
-int total = roll1 + roll2 + roll3;
-roll1 = 6;
-roll2 = 6;
-roll3 = 5;
-Console.WriteLine($"Dice rolls: {roll1}+{roll2}+{roll3} Total sum is {total}");
-
-if (total < 14)
+if (daysUntilExpiration <= 10 && daysUntilExpiration != 0)
 {
-    Console.WriteLine("You Win!");
-}
-else
-{
-    Console.WriteLine("Sorry you lose.");
-}
-
-//give a bonus to doubles
-if (roll1 == roll2 || roll1 ==roll3 || roll2 == roll3)
-{
-    if ((roll1 == roll2) && (roll2 == roll3))
+    if (daysUntilExpiration <= 5)
     {
-        Console.WriteLine("You rolled triples! +6 bonus to total!");
-        total += 6;
+        if(daysUntilExpiration == 1)
+        {
+            message = "Your subscription expires within a day! Renew now and save 20%!";
+            discountPercentage =20;
+        }
+        else
+        {
+            message = $"Your subscription expires in {daysUntilExpiration} days.Renew now and save 10%!";
+            discountPercentage = 10;    
+        }
     }
     else
     {
-        Console.WriteLine("You rolled doubles ! +2 bonus total !");
-        total +=2;    
+        message = "Your subscription will expire soon. Renew now!";        
     }
 }
+else if (daysUntilExpiration == 0)
+{
+    message = "Your subscription has expired.";
+}
+Console.WriteLine(daysUntilExpiration);
+Console.WriteLine(message);
