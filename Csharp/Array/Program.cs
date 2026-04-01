4b735c35f6b86cfa0c4bd59589184ba57d21db70
ChrisAlymaras <christos3214@gmail.com>
2026-04-01 18:25:10 +0300

    Basic array declaration and console log reversed

---
+++ Csharp/Array/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace DataStractures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] list= new int[5];
+            int input;
+            Console.WriteLine("Give 5 numbers");
+
+            for (int i=0; i<5; i++)
+            {
+                Console.WriteLine($"Enter number {i+1}:");
+                input = Convert.ToInt32(Console.ReadLine());
+                list[i] = input;
+            }
+            Array.Reverse(list);
+            Console.WriteLine(string.Join(",",list));
+            
+        }
+    }
+}
+
